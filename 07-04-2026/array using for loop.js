f2ad5f9c89ce12c8d5c323666b1149f3e66b5d61
M Mohamed Sherif <sherif22118@gmail.com>
2026-04-08 20:10:35 +0530

    Add files via upload

---
+++ 07-04-2026/array using for loop.js
@@ -0,0 +1,5 @@
+let numbers = [10, 20, 30, 40, 50];
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
---
+++ 07-04-2026/array.js
@@ -0,0 +1,19 @@
+let fruits = ["Apple", "Banana"];
+
+console.log("Original Array:", fruits);
+
+// push (add at end)
+fruits.push("Mango");
+console.log("After push:", fruits);
+
+// pop (remove from end)
+fruits.pop();
+console.log("After pop:", fruits);
+
+// unshift (add at beginning)
+fruits.unshift("Orange");
+console.log("After unshift:", fruits);
+
+// shift (remove from beginning)
+fruits.shift();
+console.log("After shift:", fruits);
