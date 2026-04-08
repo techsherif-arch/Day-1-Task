53dce081b1041bc78dbecd79870ef3ef14cbbbb9
M Mohamed Sherif <sherif22118@gmail.com>
2026-04-08 21:48:37 +0530

    Add files via upload

---
+++ 08-04-2026/greater than 10.js
@@ -0,0 +1,5 @@
+let numbers = [5, 8, 12, 3];
+
+let result = numbers.find(num => num > 10);
+
+console.log(result);
---
+++ 08-04-2026/reduce.js
@@ -0,0 +1,3 @@
+let numbers = [1, 2, 3, 4, 5];
+let sum = numbers.reduce((total, num) => total + num, 0);
+console.log(sum);
